c9b059117f24ad45b7f444fec872de9d7a6b0943
jaypolanco96 <jaypolanco96@gmail.com>
2020-10-31 21:24:08 -0400

    css

--- src/pages/swap.js
+++ src/pages/swap.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
 
 function Swap({ drizzle, drizzleState }) {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <div className="swapbox">
+        From
+        <input></input>
+        To
+        <input></input>
+        <br></br>
+        <button>Swap</button>
+      </div>
+      <style jsx>{`
+        .swapbox {
+            margin: 10px;
+            display:flex;
+            flex-direction: column;
+            justify-content: center;
+            width: 420px;
+            background: #d9d9d9;
+          box-shadow: #c5c5c5 1px 1px 0px inset;
+
+          border-radius: 30px;
+          padding: 1rem;
+        }
+      `}</style>
+    </div>
+  );
 }
 
-export default Swap
+export default Swap;
